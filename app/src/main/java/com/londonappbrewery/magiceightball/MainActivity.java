2ac33ba7ed7154188d47c9b3b6518e5dd9832c68
Calai <artur.calai@grad.ufsc..br>
2020-05-01 23:27:12 -0300

    test n1

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,15 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askbutton;
+        askbutton = (Button) findViewById(R.id.ask_button);
+
+
+        final TextView answer_display = (TextView) findViewById(R.id.answer);
+
+        final int [] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        final int [] answerArray = {
+                R.string.answer1,
+                R.string.answer2,
+                R.string.answer3,
+                R.string.answer4
+        };
+
+        askbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("8ball", "O botão foi pressionado!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(3);
+
+                answer_display.setText(answerArray[number]);
+            }
+
+
+        });
+
+
+
+
+
     }
 }
